453af45f865695c223ac452b57dc082356f52882
Faria Ahmed <82181774+LazyOlaf@users.noreply.github.com>
2022-10-13 09:09:30 +0600

    table booking backend & new edits

---
+++ web design/php/tablebooking.php
@@ -0,0 +1,25 @@
+<?php
+
+$con = mysqli_connect('localhost','root');
+
+if($con){
+	echo "Connection successful";
+}else{
+	echo "no connection";
+}
+
+mysqli_select_db($con, 'retrocafe');
+
+$name = $_POST['name'];
+$phoneno = $_POST['phoneno'];
+$date = $_POST['date'];
+$time = $_POST['time'];
+$person = $_POST['person'];
+
+$query = "insert into tablebooking (name, phoneno, date, time, person)
+values('$name','$phoneno','$date','$time','$person')";
+
+mysqli_query($con,$query);
+
+header('location:http://localhost/retro_cafe/web%20design/tableconfirmation.html');
+?>
