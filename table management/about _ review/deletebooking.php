f664ab2ede1b47bf2761881728868e7f7b370ca4
Faria Ahmed <82181774+LazyOlaf@users.noreply.github.com>
2022-12-20 16:50:19 +0600

    table management backend

---
+++ table management/about _ review/deletebooking.php
@@ -0,0 +1,10 @@
+<?php
+    $con = mysqli_connect('localhost','root');
+    $id_no=$_POST['id'];
+    if($con && $id_no>0){
+        mysqli_select_db($con, 'retrocafe');
+        mysqli_query($con,"DELETE FROM tablebooking where id=$id_no");
+        }
+mysqli_close($con);
+header('location:http://localhost/Retro_cafe/admin%20all%20page/table%20Management/about%20_%20review/index.php');
+?>
